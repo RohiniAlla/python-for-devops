15e6241a3c88c5a86b3c93790429f2bc52c8823c
RohiniAlla <133994292+RohiniAlla@users.noreply.github.com>
2024-05-03 12:05:31 -0500

    changed variablename

--- boto3/VPC/listofvpcsinacurrentregion.py
+++ boto3/VPC/listofvpcsinacurrentregion.py
@@ -2,11 +2,11 @@ import boto3
 
 def get_all_vpcs():
     ec2 = boto3.resource('ec2')
-    used_vpcs = []
+    All_vpcs = []
     for vpcs in ec2.vpcs.all():      
         # replace any non-breaking space characters with regular spaces in the vpc ID
         vpc_id = vpcs.id.replace('\u00A0', ' ')
-        used_vpcs.append(vpc_id)
-    print('ec2 list using VPCs in a current region configs %s' % used_vpcs)
+        All_vpcs.append(vpc_id)
+    print('available list using VPCs in a current region configs %s' % All_vpcs)
     
 get_all_vpcs()
